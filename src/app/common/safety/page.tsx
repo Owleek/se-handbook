fe536f93ea69415815027e10aca3ddfb2705a321
Artemii Khafizov <50587860+Owleek@users.noreply.github.com>
2026-05-21 21:46:12 +0300

    content(safety): CORS, SOP, Reffer Policy expalanation

--- src/app/common/safety/page.tsx
+++ src/app/common/safety/page.tsx
@@ -4,11 +4,11 @@ import { TileGrid, TileLink } from '@/shared/ui/Tile';
 export default function Javascript() {
   return (
     <TileGrid>
-      <div>CORS</div>
-      <div>Referrer-Policy</div>
       <div>OAuth 2.0</div>
+      <TileLink href='/common/safety/sop'>SOP / CORS</TileLink>
       <TileLink href='/common/safety/csrf'>CSRF</TileLink>
       <TileLink href='/common/safety/xss'>XSS / CSP</TileLink>
+      <TileLink href='/common/safety/refpolicy'>Referrer policy</TileLink>
     </TileGrid>
   );
 }
