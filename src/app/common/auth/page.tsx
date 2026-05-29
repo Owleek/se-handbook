077308d7232a3adcb6e6a3a76e8c88b870181f58
owleek <owleek@MacBook-Pro-owleek.local>
2026-05-29 19:26:59 +0300

    content(sso/auth2.0): login explanation

--- src/app/common/auth/page.tsx
+++ src/app/common/auth/page.tsx
@@ -4,9 +4,8 @@ import { TileGrid, TileLink } from '@/shared/ui/Tile';
 export default function Template() {
   return (
     <TileGrid>
-      <TileLink href='/common/auth/basics'>Basics</TileLink>
-      <TileLink href='/common/auth/sso'>Single Sign-On / OAuth 2.0</TileLink>
-      <TileLink href='/common/auth/resume'>Resume</TileLink>
+      <TileLink href='/common/auth/sso'>SSO versions</TileLink>
+      <TileLink href='/common/auth/login'>Login (OAuth2.0)</TileLink>
     </TileGrid>
   );
 }
