169f1377c87e282acebea8f7efa346d32b698a18
owleek <owleek@MacBook-Pro-owleek.local>
2026-05-31 22:07:24 +0300

    content(principles/solid): solid explanation

--- src/app/fundamental/code_principles/page.tsx
+++ src/app/fundamental/code_principles/page.tsx
@@ -5,6 +5,7 @@ export default function Javascript() {
   return (
     <TileGrid>
       <TileLink href='/fundamental/code_principles/oop'>OOP</TileLink>
+      <TileLink href='/fundamental/code_principles/solid'>SOLID</TileLink>
     </TileGrid>
   );
 }
