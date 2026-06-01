be6b8dab717baba98c6a15042880d765fe3f4ebe
Artemii Khafizov <50587860+Owleek@users.noreply.github.com>
2026-06-01 22:54:53 +0300

    content(front_arch/layers): draft

---
+++ src/app/fundamental/front_arch/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TileGrid, TileLink } from '@/shared/ui/Tile';
+
+export default function Template() {
+  return (
+    <TileGrid>
+      <TileLink href='/fundamental/front_arch/layers'>
+        Layers and their responsibilities
+      </TileLink>
+    </TileGrid>
+  );
+}
