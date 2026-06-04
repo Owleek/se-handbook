9f7f1f6d7162dc693828ce26d33256f3885da261
owleek <owleek@MacBook-Pro-owleek.local>
2026-06-04 23:34:12 +0300

    content(frontend arch/app): top level explanation

--- src/app/fundamental/front_arch/page.tsx
+++ src/app/fundamental/front_arch/page.tsx
@@ -4,7 +4,8 @@ import { TileGrid, TileLink } from '@/shared/ui/Tile';
 export default function Template() {
   return (
     <TileGrid>
-      <TileLink href='/fundamental/front_arch/design'>Design Systems</TileLink>
+      <TileLink href='/fundamental/front_arch/design'>Arch Design</TileLink>
+      <TileLink href='/fundamental/front_arch/deployment'>Deployment</TileLink>
     </TileGrid>
   );
 }
