3b47b6aaa5a10d31f092be6d2b4aa8865391b683
owleek <owleek@MacBook-Pro-owleek.local>
2026-06-05 21:53:53 +0300

    content(nodejs): draft

---
+++ src/app/tech/nodejs/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TileGrid, TileLink } from '@/shared/ui/Tile';
+
+export default function Javascript() {
+  return (
+    <TileGrid>
+      <TileLink href='/tech/nodejs/draft'>Draft</TileLink>
+    </TileGrid>
+  );
+}
