b01ff3c5dee76facee8510447627c42eadd69086
Artemii Khafizov <50587860+Owleek@users.noreply.github.com>
2026-06-12 21:26:10 +0300

    content(apii/graphql): draft

--- src/app/network/alp/page.tsx
+++ src/app/network/alp/page.tsx
@@ -6,11 +6,9 @@ export default function Javascript() {
     <TileGrid>
       <TileLink href='/network/alp/http'>HTTP</TileLink>
       <TileLink href='/network/apii/realtimeupdates'>Websocket</TileLink>
-      <TileLink href='/network/alp/grpc' type='ghost'>
-        gRPC
-      </TileLink>
+      <TileLink href='/network/alp/grpc'>gRPC</TileLink>
       <TileLink href='/network/alp/webrtc' type='ghost'>
-        webRTC
+        webRTC / SRTP
       </TileLink>
       <TileLink href='/network/alp/ssh' type='ghost'>
         SSH
