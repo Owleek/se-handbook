b2eeed1520ed32438585baa380914ea2b2aa56fa
owleek <owleek@MacBook-Pro-owleek.local>
2026-06-17 22:21:21 +0300

    content(next/draft): next version change log

--- src/app/tech/next/page.tsx
+++ src/app/tech/next/page.tsx
@@ -5,6 +5,20 @@ export default function Template() {
   return (
     <Note title='Next'>
       <NoteItem>
+        <p>
+          до 13 версии NEXT был обычный SSR (Pages router: запрос - генерация
+          целой html - передача клиенту - гидрация) <br />в 13 версии в
+          experimental появился RSC - react server components (App router -
+          появился streaming, передается не целиком страница а куски по
+          готовност, появились полноценные самостоятельные серверные компоненты
+          по умолчанию, до этого для получения данных нужно было отдельно от
+          компонента писать функцию getServersideProps и Next ее вызывал перед
+          вызовом самого компонента и в пропс передавал данные) <br />
+          13.4 - RSC stable <br />
+          14.0 - Server Actions Stable <br />
+          15.0 - Изменения в кэшировании и async API <br />
+          16.0 - Turbopack и дальнейшее развитие App Router
+        </p>
         <p>Причины</p>
         <p>Генерация / Flow / technologies</p>
         <p>Hydration</p>
