9cae12cacae5bfcb995920890de7c3f6ed230bbd
Artemii Khafizov <50587860+Owleek@users.noreply.github.com>
2026-06-25 20:31:54 +0300

    content(react/draft): render draft & use-cases draft

--- src/app/tech/react/page.tsx
+++ src/app/tech/react/page.tsx
@@ -4,9 +4,8 @@ import { TileGrid, TileLink } from '@/shared/ui/Tile';
 export default function Javascript() {
   return (
     <TileGrid>
-      <TileLink href='/tech/js'>JS</TileLink>
-      <TileLink href='/tech/next'>NextJS</TileLink>
-      <TileLink href='/tech/react'>React</TileLink>
+      <TileLink href='/tech/react/render'>Render & Reconciliation</TileLink>
+      <TileLink href='/tech/react/usecases'>Use cases</TileLink>
     </TileGrid>
   );
 }
