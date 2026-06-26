2e64bd95154140780a9a1bd1d75bc2d9e60bc8f3
Artemii Khafizov <50587860+Owleek@users.noreply.github.com>
2026-06-26 23:14:38 +0300

    content(react/render): edit draft

--- src/app/tech/react/render/page.tsx
+++ src/app/tech/react/render/page.tsx
@@ -10,6 +10,26 @@ export default function Template() {
     <Note title='Render & Reconciliation'>
       <NoteItem>
         <p>1. RE Tree & Fiber Tree</p>
+        <pre>
+          {`
+            1. React cоздает React Elements
+
+            2. формирует Fiber nodes
+            
+            3. setState()
+                    
+            4. создает WorkInProgress Fiber
+                    
+            5. Выполняется render компонента
+              (вызывается функция компонента с НОВЫМИ props)
+                    ↓
+            6. Reconciliation
+              (сравнение нового React Tree со старым)
+                    ↓
+            7. Commit
+              (изменение реального DOM)
+            `}
+        </pre>
         <p>2. Fiber intro</p>
         <p>3. Effects processes / prioritization</p>
         <p>4. curr tree / wip tree</p>
