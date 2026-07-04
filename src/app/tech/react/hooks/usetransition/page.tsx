6daa1589eb2b7af3cf9d92ae504acec145a44591
owleek <owleek@MacBook-Pro-owleek.local>
2026-07-05 00:18:18 +0300

    content(react): useTransition & useDefferedValue explanation

---
+++ src/app/tech/react/hooks/usetransition/page.tsx
@@ -0,0 +1,53 @@
+import { Note, NoteItem } from '@/shared/ui/Note';
+
+export default function Template() {
+  return (
+    <Note title='useTransition & useDefferedValue'>
+      <NoteItem>
+        <p>
+          Используя startTransition и useDefferedValue мы говорим React что это
+          действие не первой важности и его выполнение можно отложить если есть
+          более важные задачи. (Это возможно благодаря Concurrent rendering)
+        </p>
+        <p>
+          useTransition - используем тогда когда мы контролируем источник
+          изменений, а когда не контролируем - useDefferedValue
+        </p>
+        <p>
+          Используем хук useTransition чтобы получить isPending и реагировать на
+          тяжелые пересчеты таблиц, а если рендер быстрый то просто импортируем
+          startTransition без хука useTransition
+        </p>
+        <pre>
+          {`
+    const [isPending, startTransition] = useTransition();
+    const handleClick = () => {
+       doImportantThings()
+       ....      
+    
+       startTransition(() => {
+            менее приоритетная задача - ее можно отложить для более важной
+            setTab("Analytics");
+        });
+    }
+ 
+    <Tabs loading={isPending} />
+          `}
+        </pre>
+        <br />
+        <p>
+          useDefferedValue почти не используют, а используют либо useDebounce
+          либо startTransition
+        </p>
+        <pre>
+          {`
+    const deferredValue = useDeferredValue(value);
+    const filtered = useMemo(() => {
+        return heavyFilter(products, deferredValue);
+    }, [deferredValue]);
+          `}
+        </pre>
+      </NoteItem>
+    </Note>
+  );
+}
