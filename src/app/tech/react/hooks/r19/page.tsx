89502d6c86e26a6cc4502f310448df7a02df6547
owleek <owleek@MacBook-Pro-owleek.local>
2026-07-09 20:29:56 +0300

    content(react): R19 hooks

--- src/app/tech/react/hooks/r19/page.tsx
+++ src/app/tech/react/hooks/r19/page.tsx
@@ -41,8 +41,10 @@ function User({ promise }) {
 import { Suspense } from "react";
 import User from "./User";
 
-/ важно чтобы промис был создан вне компонента, иначе он будет создаваться при каждом рендере компонента
-/ но тогда будет засорятся глобальное пространство, а значит нельзя использовать
+/ важно чтобы промис был создан вне компонента, 
+/ иначе он будет создаваться при каждом рендере компонента
+/ но тогда будет засорятся глобальное пространство,
+/ а значит нельзя использовать
 
 const userPromise = fetch("/api/user").then(r => r.json());
 
@@ -59,17 +61,165 @@ export default function App() {
       </Note>
       <Note title='useOptimistic'>
         <NoteItem>
-          <div></div>
+          <p>
+            Зачастую - оптимистическое обновление, кеширование, дедубликация
+            запросов решаются библиотекой RTK
+          </p>
+          <p>
+            useOptimistic почти не используют, поскольку серверные экшены почти
+            не используются, а обычный запрос + инвалидация next все усложняет,
+            проще не использовать
+          </p>
+          <p>
+            useOptimistic — это не состояние, а overlay (наложение) над
+            настоящим состоянием которое может приходить откуда угодно
+          </p>
+          <pre>
+            {`
+const [optimisticLikes, addLike] = useOptimistic(
+    trueState,
+    (currentState, like) => {
+        return [...currentState, like];
+    }
+);
+            `}
+          </pre>
+          <p>В случаях когда происходит like - unlike - like</p>
+          <p>
+            Скорее всего оптимистичекое состонияе будут отличаться от
+            серверного, для этого можно использовать некоторые подходы
+          </p>
+          <p>например:</p>
+          <p>
+            при очередном вызове оптимистического обновления, старый useref
+            controller абортитить controller.abort() и присваивать useref новый
+            new AbortContoller()
+          </p>
+          <br />
+          <p>Пример использовать AbortContoller</p>
+          <p>
+            т.е в контекст запроса, по мимо метода запроса, хеадерс и бади,
+            передаем еще и signal: controller.signal для браузера
+          </p>
+          <pre>
+            {`
+const controller = new AbortController();
+fetch("/api/users", {
+  signal: controller.signal,
+});
+              `}
+          </pre>
+          <br />
+          <p>
+            Другой способ, использовать обновление истинного состояния только
+            для последнего запроса используя счетчик
+          </p>
+          <pre>
+            {`
+async function handleClick(nextValue: boolean) {
+  // Пользователь сразу увидел изменение
+  setOptimisticValue(nextValue);
+
+  // Получили id текущего запроса
+  const requestId = ++requestIdRef.current;
+
+  try {
+    const result = await api.do(nextValue);
+
+    // Пока ждали сервер, пользователь уже нажал еще раз
+    if (requestId !== requestIdRef.current) {
+      return;
+    }
+
+    // Обновляем настоящее состояние
+    setTrueValue(result.value);
+
+  } catch {
+    // Это уже не последний запрос?
+    if (requestId !== requestIdRef.current) {
+      return;
+    }
+
+    // Показываем ошибку.
+    // Серверное состояние не изменилось,
+    // поэтому optimistic UI автоматически исчезнет.
+    toast.error("Не удалось поставить лайк");
+  }
+}
+              `}
+          </pre>
         </NoteItem>
       </Note>
       <Note title='useFormStatus'>
         <NoteItem>
-          <div></div>
+          <p>
+            useFormStatus — позволяет любому компоненту внутри form узнать
+            текущее состояние отправки этой формы, не передавая это состояние
+            через props
+          </p>
+          <p>
+            используется для простых форм без использования библиотек, поскольку
+            в бибилиотеках уже есть эта реализация
+          </p>
+          <pre>
+            {`
+function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button disabled={pending}>
+      {pending ? "Saving..." : "Save"}
+    </button>
+  );
+}
+            `}
+          </pre>
         </NoteItem>
       </Note>
       <Note title='useActionState'>
         <NoteItem>
-          <div></div>
+          <p>
+            useActionState - отвечает на вопрос Какой результат вернула отправка
+            формы и автоматически обновлять UI
+          </p>
+          <pre>
+            {`
+'use client';
+import { useActionState } from 'react';
+import { loginUser } from '@/app/actions'; // Это Server Action (серверная функция)
+
+export default function LoginForm() {
+  // Передаем серверную функцию и начальное состояние (ошибок нет)
+  const [state, formAction, isPending] = useActionState(loginUser, { error: null });
+
+  return (
+    <form action={formAction}>
+      <input type="email" name="email" required />
+      <input type="password" name="password" required />
+      
+      {/* Выводим ошибку, если сервер её вернул */}
+      {state?.error && <p style={{ color: 'red' }}>{state.error}</p>}
+      
+      {/* Автоматически блокируем кнопку во время отправки */}
+      <button type="submit" disabled={isPending}>
+        {isPending ? 'Вход...' : 'Войти'}
+      </button>
+    </form>
+  );
+}
+
+"use server";
+export async function loginUser(prevState, formData) {
+  const email = formData.get("email");
+
+  if(error) return {error: 'something wrong'}
+
+  return {
+    message: 'asdasdas',
+  };
+}
+            `}
+          </pre>
         </NoteItem>
       </Note>
     </React.Fragment>
