97361d829a05fa4ece25047f9004ad148ad0198f
owleek <owleek@MacBook-Pro-owleek.local>
2026-07-14 23:26:04 +0300

    content(algorythm): big O draft

--- src/app/fundamental/page.tsx
+++ src/app/fundamental/page.tsx
@@ -7,6 +7,7 @@ export default function Template() {
       <TileLink href='/fundamental/code_approaches'>Code Approaches</TileLink>
       <TileLink href='/fundamental/code_principles'>Code Principles</TileLink>
       <TileLink href='/fundamental/front_arch'>Front Architecture</TileLink>
+      <TileLink href='/fundamental/algorythm'>Algorythm</TileLink>
     </TileGrid>
   );
 }
