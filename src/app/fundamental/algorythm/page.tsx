e7e31629542b54e9a951f7b00f471389515c4397
owleek <owleek@MacBook-Pro-owleek.local>
2026-07-18 22:47:23 +0300

    content(algorythm): sort draft

--- src/app/fundamental/algorythm/page.tsx
+++ src/app/fundamental/algorythm/page.tsx
@@ -5,6 +5,7 @@ export default function Template() {
   return (
     <TileGrid>
       <TileLink href='/fundamental/algorythm/basics'>Basics</TileLink>
+      <TileLink href='/fundamental/algorythm/sort'>Sort</TileLink>
     </TileGrid>
   );
 }
