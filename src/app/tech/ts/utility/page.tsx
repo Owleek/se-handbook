a36ccdcbcec500bffe43517c7690c8580762286d
owleek <owleek@MacBook-Pro-owleek.local>
2026-08-04 19:27:42 +0300

    content(ts): utility types draft

--- src/app/tech/ts/utility/page.tsx
+++ src/app/tech/ts/utility/page.tsx
@@ -3,7 +3,12 @@ import { Note, NoteItem } from '@/shared/ui/Note';
 export default function Template() {
   return (
     <>
-      <Note title='Omit'>
+      <Note title='Partial'>
+        <NoteItem>
+          <p></p>
+        </NoteItem>
+      </Note>
+      <Note title='Required'>
         <NoteItem>
           <p></p>
         </NoteItem>
@@ -12,7 +17,66 @@ export default function Template() {
         <NoteItem>
           <p></p>
         </NoteItem>
-        . <br />. <br /> .
+      </Note>
+      <Note title='Omit'>
+        <NoteItem>
+          <p></p>
+        </NoteItem>
+      </Note>
+      <Note title='Record'>
+        <NoteItem>
+          <p></p>
+        </NoteItem>
+      </Note>
+      <Note title='Readonly'>
+        <NoteItem>
+          <p></p>
+        </NoteItem>
+      </Note>
+      <Note title='Exclude'>
+        <NoteItem>
+          <p></p>
+        </NoteItem>
+      </Note>
+      <Note title='Extract'>
+        <NoteItem>
+          <p></p>
+        </NoteItem>
+      </Note>
+      <Note title='NonNullable'>
+        <NoteItem>
+          <p></p>
+        </NoteItem>
+      </Note>
+      <Note title='ReturnType'>
+        <NoteItem>
+          <p></p>
+        </NoteItem>
+      </Note>
+      <Note title='Parameters'>
+        <NoteItem>
+          <p></p>
+        </NoteItem>
+      </Note>
+      <Note title='Awaited'>
+        <NoteItem>
+          <p></p>
+        </NoteItem>
+      </Note>
+      <Note title='Other'>
+        <NoteItem>
+          <p>
+            InstanceType <br />
+            ConstructorParameters <br />
+            ThisParameterType <br />
+            OmitThisParameter <br />
+            ThisType <br />
+            Uppercase <br />
+            Lowercase <br />
+            Capitalize <br />
+            Uncapitalize
+          </p>
+        </NoteItem>
       </Note>
     </>
   );
