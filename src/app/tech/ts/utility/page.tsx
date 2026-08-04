a5815dabfa9c6246c4d1864332329dd3e87dfb34
owleek <owleek@MacBook-Pro-owleek.local>
2026-08-04 22:50:23 +0300

    content(ts): Partial<T> explanation

--- src/app/tech/ts/utility/page.tsx
+++ src/app/tech/ts/utility/page.tsx
@@ -5,7 +5,82 @@ export default function Template() {
     <>
       <Note title='Partial'>
         <NoteItem>
-          <p></p>
+          <p>Делает все свойства типа необязательными</p>
+          <pre>
+            {`
+Пример:
+
+// В данном случае все свойства обязательны
+// нужно например для создания пользователя
+
+interface User {
+    id: number
+    name: string
+    email: string
+    age: number
+}
+
+// Но если нам нужно обновить некоторые поля
+// тогда мы при patch удобен Partial<T>
+
+type UpdateUserDto = Partial<User>
+
+const user: UpdateUserDto = {}
+// user
+    // .id?
+    // .name?
+    // .email?
+    // .age?
+`}
+          </pre>
+          <br />
+          <p>
+            Partial делает все поля необязательными, нельзя сделать одно или
+            выборочно, для этого есть комбинация с комбинации Pick и Omit
+          </p>
+          <p>
+            Partial делает поля необязательными только на верхнем уровне
+            (shallow/поверхностное)
+          </p>
+          <p>
+            Т.е если будет вложенный объект, то поля вложенного объекта будут
+            обязательными
+          </p>
+          <p>Для этого нужно писать собственную реализацию DeepPartial</p>
+          <br />
+          <br />
+          <p>Внутренняя реализация Patial</p>
+          <pre>
+            {`
+type Partial<T> = {
+    [K in keyof T]?: T[K];
+}
+
+keyof T - создает union ключей: "id" | "name" | "email" | "age"
+
+K in - keyof T проходит по каждому ключу
+
+После in должен стоять union ключей: "a" | "b" либо 1 | 5 либо User
+
+type T = {
+    [K in "name" | "age"]: string
+}
+
+превратится в
+type T = {
+    name: string
+    age: string
+}
+
+K = "id",
+K = "name",
+....
+
+T[K] берет тип ключа
+"id"?: number
+....
+`}
+          </pre>
         </NoteItem>
       </Note>
       <Note title='Required'>
