c17858e48e2de5075c9f94ddfe8bad5fbe5c90ed
owleek <owleek@MacBook-Pro-owleek.local>
2026-08-15 20:13:29 +0300

    cleanup

--- src/app/tech/js/page.tsx
+++ src/app/tech/js/page.tsx
@@ -7,9 +7,6 @@ export default function Javascript() {
       <TileLink href='/tech/js/modules'>Modules</TileLink>
       <TileLink href='/tech/js/lexenv'>Lexical Environment</TileLink>
       <TileLink href='/tech/js/increment'>++x--</TileLink>
-      <TileLink href='/tech/js/promise' type='ghost'>
-        Promise
-      </TileLink>
     </TileGrid>
   );
 }
