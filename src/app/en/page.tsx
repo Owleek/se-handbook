e5cbed54c547b6e5a514d5179bf87fbf3ab21392
Artemii Khafizov <50587860+Owleek@users.noreply.github.com>
2026-09-14 23:56:48 +0300

    en: have/has/had

---
+++ src/app/en/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TileGrid, TileLink } from '@/shared/ui/Tile';
+
+export default function Javascript() {
+  return (
+    <TileGrid>
+      <TileLink href='/tech/js'>JS</TileLink>
+      <TileLink href='/tech/next'>NextJS</TileLink>
+      <TileLink href='/tech/react'>React</TileLink>
+      <TileLink href='/tech/ts'>TS</TileLink>
+      <TileLink href='/tech/cli'>Linux Cli</TileLink>
+    </TileGrid>
+  );
+}
